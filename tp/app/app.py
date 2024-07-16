ea074a5be267f8706afbf0af7476b259a27dc675
VeroSpi <163481771+VeroSpi@users.noreply.github.com>
2024-07-16 17:42:28 -0300

    agregado de enlaces html, algunas imagenes y cambios en css

--- tp/app/app.py
+++ tp/app/app.py
@@ -1,8 +1,9 @@
 #  .venv\Scripts\activate
-# cd app --> flask --app app run --debug
+# cd app --> flask --app app 
+# run --debug
 
 from flask import Flask, render_template, request, redirect, url_for
-from controller_db import conexionMySQL,ver_contactos
+from controller_db import *
 
 app = Flask(__name__)
 
@@ -12,13 +13,76 @@ def Inicio():
     title = "Inicio"
     return render_template("index.html", titulo=title)
 
+# @app.route("/contactosagendados/<int:id>")
+# def datacontactos(id):
+#     title="Reservas"
+#     return render_template("contactosagendados.html",title=title,contactos=contactos[id])
+
+
 # Sección Contacto
 @app.route("/contacto")
 def Contacto():
     contacto = ver_contactos()
     title = "contacto"
-    return render_template("contacto.html", title=title,
-    contacto =contacto)
+    return render_template("contacto.html", title=title, 
+    contacto=contacto)
+
+# Insert
+# cargar el contacto
+@app.route("/cargar_contacto")
+def agregar_contacto():
+    title= "Cargar Contacto"
+    return render_template("nuevo_contacto.html", title=title)
+
+# enviar el  formulario de contacto por el metodo post
+@app.route("/guardar_contacto", methods=["POST"])
+def guardar_contacto():
+    title= "Guardar Contacto"
+    print(request.form)
+    nombre = request.form["nombre"]
+    apellido = request.form["apellido"]
+    email = request.form["email"]
+    telefono = request.form["telefono"]
+    fecha_evento = request.form["fecha_evento"]
+    cargar_contacto(nombre, apellido,email,telefono,fecha_evento)
+    return  redirect("/contacto")
+
+# update 
+@app.route("/editar_contacto/<int:id>")
+def editar_contacto(id):
+    title = "Editar contacto"
+    contacto_por_id = obtener_contacto_por_id(id)
+    return render_template("editar_contacto.html", title=title, contacto=contacto_por_id)
+
+# @app.route("/editar_contacto", methods=["GET","POST"])
+# def guardar_edicion_contacto():
+#     id_edit = request.form["id"]
+#     nombre = request.form["nombre"]
+#     apellido = request.form["apellido"]
+#     email = request.form["email"]
+#     telefono = request.form["telefono"]
+#     fecha_evento = request.form["fecha_evento"]
+#     resp = actualizar_contacto(id_edit,nombre, apellido,email,telefono,fecha_evento)
+#     return  redirect("/contacto")
+
+@app.route("/editar_contacto", methods=["POST"])
+def guardar_edicion_contacto():
+    id = request.form.get("id")
+    nombre = request.form.get("nombre")
+    apellido = request.form.get("apellido")
+    email = request.form.get("email")
+    telefono = request.form.get("telefono")
+    fecha_evento = request.form.get("fecha_evento")
+
+    actualizar_contacto(id, nombre, apellido, email, telefono, fecha_evento)
+    return redirect("/contacto")
+    
+# delete
+@app.route("/borrar_contacto/<int:id>")
+def delte_contacto(id):
+    eliminar_contacto(id)
+    return redirect("/contacto")
+
 
 # Sección Conóceme
 @app.route("/conoceme")
@@ -35,81 +99,6 @@ def galeria():
 if __name__ == "__main__":
     app.run(debug=True)
 
-########
-
-# # Ruta para mostrar todos los contactos
-# @app.route('/bd-contactos')
-# def contactos():
-#     title = contactos
-#     conn = mysql.conexion.connect(**db_config)
-#     cursor = conn.cursor(dictionary=True)
-#     cursor.execute("SELECT * FROM contactos")
-#     contactos = cursor.fetchall()
-#     cursor.close()
-#     conn.close()
-#     return render_template('contactos.html', title = title)
-
-# # Ruta para manejar el formulario de contacto
-# @app.route('/contacto', methods=['GET', 'POST'])
-# def contacto():
-#     if request.method == 'POST':
-#         nombre = request.form['nombre']
-#         email = request.form['email']
-#         telefono = request.form['telefono']
-#         fecha_evento = request.form['fecha_evento']
-#         mensaje = request.form['mensaje']
-        
-#         # Insertar datos en la base de datos
-#         conn = mysql.connector.connect(**db_config)
-#         cursor = conn.cursor()
-#         cursor.execute("INSERT INTO contactos (nombre, email, telefono, fecha_evento, mensaje) VALUES (%s, %s, %s, %s, %s)", 
-#                        (nombre, email, telefono, fecha_evento, mensaje))
-#         conn.commit()
-#         cursor.close()
-#         conn.close()
-        
-#         return redirect(url_for('contactos'))
-#     return render_template('contacto.html')
-
-# # Ruta para editar un contacto
-# @app.route('/editar/<int:id>', methods=['GET', 'POST'])
-# def editar(id):
-#     conn = mysql.connector.connect(**db_config)
-#     cursor = conn.cursor(dictionary=True)
-#     cursor.execute("SELECT * FROM contactos WHERE id = %s", (id,))
-#     contacto = cursor.fetchone()
-    
-#     if request.method == 'POST':
-#         nombre = request.form['nombre']
-#         email = request.form['email']
-#         telefono = request.form['telefono']
-#         fecha_evento = request.form['fecha_evento']
-#         mensaje = request.form['mensaje']
-        
-#         cursor.execute("""
-#             UPDATE contactos
-#             SET nombre = %s, email = %s, telefono = %s, fecha_evento = %s, mensaje = %s
-#             WHERE id = %s
-#         """, (nombre, email, telefono, fecha_evento, mensaje, id))
-#         conn.commit()
-#         return redirect(url_for('contactos'))
-    
-#     cursor.close()
-#     conn.close()
-#     return render_template('editar.html', contacto=contacto)
-
-# # Ruta para eliminar un contacto
-# @app.route('/eliminar/<int:id>')
-# def eliminar(id):
-#     conn = mysql.connector.connect(**db_config)
-#     cursor = conn.cursor()
-#     cursor.execute("DELETE FROM contactos WHERE id = %s", (id,))
-#     conn.commit()
-#     cursor.close()
-#     conn.close()
-#     return redirect(url_for('contactos'))
-
-#######
 
 
 
--- tp/app/controller_db.py
+++ tp/app/controller_db.py
@@ -18,3 +18,62 @@ def ver_contactos():
         conexion.commit()
         conexion.close()
         return result
+    
+# create -> INSERT
+def cargar_contacto(nombre, apellido,email,telefono,fecha_evento):
+    # conexion:
+    conexion = conexionMySQL()
+    # consulta db:
+    with conexion.cursor() as cursor:
+        # Read a single record
+        sql = "INSERT INTO contactos (nombre, apellido,email,telefono,fecha_evento) VALUES (%s,%s,%s,%s,%s)"
+        cursor.execute(sql,(nombre, apellido,email,telefono,fecha_evento))
+
+        #  se procesan los resultados:
+        result = cursor.lastrowid
+        # cerrar la conexion:
+        conexion.commit()
+        conexion.close()
+        return result
+    
+# update -> 1)
+def obtener_contacto_por_id(id):
+    # conexion:
+    conexion = conexionMySQL()
+    # consulta db:
+    with conexion.cursor() as cursor:
+        # Read a single record
+        sql = "SELECT * FROM contactos WHERE id = %s"
+        cursor.execute(sql,(id))
+        #  se procesan los resultados:
+        result = cursor.fetchone()
+
+        # cerrar la conexion:
+    conexion.commit()
+    conexion.close()
+    return result
+
+# update -> 2)
+def actualizar_contacto(nombre, apellido,email,telefono,fecha_evento,id):
+    # conexion:
+    conexion = conexionMySQL()
+    # consulta db:
+    with conexion.cursor() as cursor:
+        # consulta de acltualizacion
+        cursor.execute("UPDATE contactos SET nombre = %s, apellido = %s, email = %s, telefono = %s, fecha_evento = %s WHERE id = %s",(nombre, apellido, email, telefono, fecha_evento, id))
+        #  se procesan los resultados:
+        result = cursor
+        # cerrar la conexion:
+        conexion.commit()
+        conexion.close()
+        return result
+    
+# borrar -> delete 
+def eliminar_contacto(id):
+    conexion = conexionMySQL()
+    with conexion.cursor() as cursor:
+        cursor.execute("DELETE FROM contactos WHERE id = %s", (id))
+        result = cursor
+    conexion.commit()
+    conexion.close()
+    return result
